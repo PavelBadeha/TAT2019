911ce00fb49328d94cb5b94c853269142e317e93
Pavel Badeha <pbadzelha@mail.ru>
2019-09-21 19:26:13 +0300

    Add Dev-0

---
+++ Dev-0/Dev-0/EntryPoiny.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dev_0
+{
+    /// <summary>
+    ///The main class of the program    
+    /// </summary>
+    class EntryPoiny
+    {
+        /// <summary>
+        /// The entry point.
+        /// </summary>
+        static void Main()
+        {
+            //Method Testing
+            Console.WriteLine(NumberOfRepeatedLetters.Calculate("asdffrtttsssssrt"));
+        }
+    }
+}
---
+++ Dev-0/Dev-0/NumberOfRepeatedLetters.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Dev_0
+{   /// <summary>
+    /// The class that contains a method which returns maximum number of repeated consecutive letters
+    /// </summary>
+    class NumberOfRepeatedLetters
+    {
+        /// <summary>
+        /// The method returns maximum number of repeated consecutive letters
+        /// </summary>
+        public static int Calculate(string str)
+        {
+            int count = 1;
+            int max = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i + 1 < str.Length && str[i].Equals(str[i + 1]))
+                {
+                    count++;
+                }
+                else
+                    count = 1;
+                if (max < count)
+                    max = count;
+            }
+            return max;
+        }
+    }
+}
