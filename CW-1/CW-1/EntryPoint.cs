1f277e93ffc887a68447523bd2d7ae164761665b
Pavel Badeha <pbadzekha@mail.ru>
2019-10-01 14:06:08 +0300

    CW-1

---
+++ CW-1/CW-1/EntryPoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CW_1
+{
+    /// <summary>
+    /// Main class of the program
+    /// </summary>
+    class EntryPoint
+    {
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        static void Main()
+        {
+            Point A = new Point(0,0);
+            Point B = new Point(2, 4);
+            Point C = new Point(4, 0);
+
+            Point A1= new Point(1, 0);
+            Point B1= new Point(2, 4);
+            Point C1= new Point(4, 0);
+
+            List<Triangle> triangles = new List<Triangle>() {new Triangle(A, B, C)
+                                                            ,new Triangle(A1,B1,C1)
+                                                            ,new Triangle(B,A,C)};
+
+           Console.WriteLine(Triangle.IsThereTwoIdenticalTriangles(triangles)); 
+
+        }
+    }
+}
---
+++ CW-1/CW-1/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CW_1
+{
+    /// <summary>
+    /// Class Point
+    /// </summary>
+    class Point
+    {
+        /// <summary>
+        /// Point X
+        /// </summary>
+        public double pointX { get; set; }
+        /// <summary>
+        /// Point Y
+        /// </summary>
+        public double pointY { get; set; }
+
+        /// <summary>
+        /// Constructor of the class 
+        /// </summary>
+        /// <param name="pointX">Point X</param>
+        /// <param name="pointY">Point Y</param>
+        public Point(double pointX, double pointY)
+        {
+            this.pointX = pointX;
+            this.pointY = pointY;
+        }
+
+        /// <summary>
+        /// Method that calculate the distance between two points
+        /// </summary>
+        /// <param name="A">Second point</param>
+        /// <returns>The distance</returns>
+        public double GetDistance(Point A)
+        {
+            return Math.Pow(Math.Pow(pointX - A.pointX, 2) + Math.Pow(pointY - A.pointY, 2), 0.5);
+        }
+    }
+}
