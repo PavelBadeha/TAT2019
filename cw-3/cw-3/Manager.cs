8e3aad303527e131eaf7c9cbc3fda22166c6301f
Pavel Badeha <pbadzekha@mail.ru>
2019-11-11 12:34:22 +0300

    cw-3 (Matrix,delegate and event)

---
+++ cw-3/cw-3/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace cw_3
+{
+    /// <summary>
+    /// Class of manager
+    /// </summary>
+    class Manager
+    {
+        /// <summary>
+        /// Method that display changes and will add to matrix's event
+        /// </summary>
+        public void DisplayChanges()
+        {
+            Console.WriteLine("Value changes");
+        }
+    }
+}
---
+++ cw-3/cw-3/Matrix.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw_3
+{ 
+    class Matrix <Nullable> 
+    {
+        private Nullable[] values;
+        public delegate void changedDelegate();
+        public event changedDelegate OnChanged;
+        public int Size { get; }
+        public Matrix(int size,Nullable[] values)
+        {
+            Size = size;
+            this.values = new Nullable[2*size];
+            this.values = values;
+        }
+        public Nullable this[int row,int column]
+        {
+            get
+            {
+                return values[Size*row+column]; 
+            }
+            set
+            {
+                values[Size*row+column] = value;
+                OnChanged?.Invoke();
+            }
+        }
+        public bool IsDiagonal()
+        {
+            for(int i=0;i<Size;i++)
+            {
+               for(int j=;j<Size;j++)
+                {
+                    if(i!=j && this[i,j]!=null)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
