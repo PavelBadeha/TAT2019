3fadfd824280d2fb10691b2c4eda6da2a3509e61
Pavel Badeha <pbadzekha@mail.ru>
2019-11-16 16:40:27 +0300

    Commit without unit tests

---
+++ Dev-2/Dev-2/EntryPoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev_2
+{
+    /// <summary>
+    /// Entry Point
+    /// </summary>
+    class EntryPoint
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        static void Main()
+        {
+            while(true)
+            {
+                try
+                {
+                    StringAnalyzer analyzer = new StringAnalyzer();
+                    Console.WriteLine("Max identical digits: " + analyzer.MaxOfIdenticalConsecutiveDigits());
+                    Console.WriteLine("Max identical latins: " + analyzer.MaxOfIdenticalConsecutiveLatinSymbols());
+                    Console.WriteLine("Max not identical symbols: " + analyzer.MaxOfNotIdenticalConsecutiveSymbols());
+                    break;
+                }                    
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Dev-2/Dev-2/StringAnalyzer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+
+namespace Dev_2
+{
+    class StringAnalyzer
+    {
+        /// <summary>
+        /// String that need to analyze
+        /// </summary>
+        private string str = string.Empty;
+
+        /// <summary>
+        /// Class constructor without params
+        /// </summary>
+        public StringAnalyzer()
+        {
+            Console.WriteLine("Enter the string:");
+            str = Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Returns the maximum number of unequal consecutive characters in a string
+        /// </summary>
+        public int MaxOfNotIdenticalConsecutiveSymbols()
+        {
+            int result = 1;
+            int buff = 1;
+
+            for (int i = 0; i < str.Length; i++) 
+            {
+                if( i < str.Length - 1 && str[i + 1] != str[i])
+                {
+                    buff++;
+                }
+                else
+                {
+                    if (buff > result)
+                    {
+                        result = buff;
+                    }
+                    buff = 1;
+                }
+            }
+            if (str == string.Empty)
+            {
+                return 0;
+            }
+            return result;                   
+        }
+
+        /// <summary>
+        /// Returns the maximum number of identical consecutive digits in a string
+        /// </summary>
+        public int MaxOfIdenticalConsecutiveDigits()
+        {
+            int result = 1;
+            int buff = 1;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i < str.Length - 1 && str[i + 1] == str[i] && (str[i] > 47 && str[i] < 58))
+                {
+                    buff++;
+                }
+                else
+                {
+                    if (buff > result)
+                    {
+                        result = buff;
+                    }
+                    buff = 1;
+                }
+            }
+
+            if (!str.Any(x => x > 47 && x < 58))
+            {
+                return 0;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the maximum number of identical consecutive Latin characters in a string
+        /// </summary>
+        public int MaxOfIdenticalConsecutiveLatinSymbols()
+        {
+            int result = 1;
+            int buff = 1;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((i < str.Length - 1 && str[i + 1] == str[i])
+                    && ( str[i] > 64 && str[i] < 91 || (str[i] > 96 && str[i] < 123)))
+                {
+                    buff++;
+                }
+                else
+                {
+                    if (buff > result)
+                    {
+                        result = buff;
+                    }
+                    buff = 1;
+                }
+            }
+
+            if (!str.Any(x => x > 64 && x < 91 || (x > 96 && x < 123)))
+            {
+                return 0;
+            }
+            return result;
+        }
+    }
+}
