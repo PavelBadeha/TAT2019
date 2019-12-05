4ed5f866b064628d92a316473f7e0864d583fb1b
Pavel Badeha <pbadzekha@mail.ru>
2019-12-05 19:39:32 +0300

    Cw-5

---
+++ CW-5/CW-5/Parts/Engine.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace CW_5
+{
+    class Engine
+    {
+        private float _power;
+        public float Power
+        {
+            get
+            {
+                return _power;
+            }
+            private set
+            {
+                if (value>=0)
+                {
+                    _power = value;
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
+        }
+        private float _capacity;
+        public float Capacity
+        {
+            get
+            {
+                return _capacity;
+            }
+            private set
+            {
+                if (value >= 0)
+                {
+                    _capacity = value;
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
+        }
+
+        private string _type;
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+            private set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _type = value;
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
+        }
+
+        private int _serialNumber;
+        public int SerialNumber
+        {
+            get
+            {
+                return _serialNumber;
+            }
+            private set
+            {
+                if (value >= 0)
+                {
+                    _serialNumber = value;
+                }
+            }
+        }
+        public Engine(float power,float capacity,string type,int serialNumber)
+        {
+            Power = power;
+            Capacity = capacity;
+            Type = type;
+            SerialNumber = serialNumber;
+        }
+        public override string ToString()
+        {
+            return "Engine:\nPower: " +
+                    Power.ToString() +
+                    " ,Capacity: " +
+                    Capacity.ToString() +
+                    " ,Type: " +
+                    Type +
+                    " ,Serial Number: " +
+                    SerialNumber.ToString();
+        }
+    }
+}
---
+++ CW-5/CW-5/Parts/Transmission.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace CW_5
+{
+    class Transmission
+    {
+        private string _type;
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+            private set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _type = value;
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
+        }
+        private int _gearAmount;
+        public int GearAmount
+        {
+            get
+            {
+                return _gearAmount;
+            }
+            private set
+            {
+                if (value > 0)
+                {
+                    _gearAmount = value;
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
+        }
+
+        private string _brand;
+        public string Brand
+        {
+            get
+            {
+                return _brand;
+            }
+            private set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _brand = value;
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
+        }
+        public Transmission (string type,int gearAmount,string brand)
+        {
+            Type = type;
+            GearAmount = gearAmount;
+            Brand = brand;
+        }
+        public override string ToString()
+        {
+            return "Transmission:\nType: " +
+                    Type +
+                    " ,Gear Amount: " +
+                    GearAmount.ToString() +
+                    " ,Brand :" +
+                    Brand;
+        }
+    }
+}
